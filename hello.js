0c28375051a8bdd76174cf8ee4577d88d7cf0d1a
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-01-16 20:49:10 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,45 @@
+
+var number = -5;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+// var number = 5.55555;
+// var result = Math.round(number);
+// console.log(result);
+
+// var number = 5.255;
+// var result = Math.ceil(number);
+// console.log(result);
+// var number = 5.955;
+// var result = Math.floor(number);
+// console.log(result);
+// var number = 5.255;
+// var result = Math.random(number);
+// console.log(result);
+// var number = Math.random() * 100;
+// var result = Math.round(number);
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
